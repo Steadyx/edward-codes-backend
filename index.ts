a9efea966b62665810cde8df0529b566474ba774
Edward Kemp <edwardlanekemp@gmail.com>
2023-08-16 02:11:42 +0100

    Update PORT variable to use environment variable if available

--- index.ts
+++ index.ts
@@ -135,7 +135,7 @@ fetchAllConfigurations().then((config) => {
 
   app.use("/api", apiRouter);
 
-  const PORT = 3001;
+  const PORT = process.env.PORT || 5000;
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
